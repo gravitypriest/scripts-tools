ab28fd09b635e752a342f79996cf9a1f2bd327e1
Jeremy Crafts <craftsje@gmail.com>
2015-09-20 14:32:09 -0400

    add splitting and offset

--- zero-bd-chapters.py
+++ zero-bd-chapters.py
@@ -1,11 +1,10 @@
 '''
-Zero out the first chapter in a Blu-ray
-chapters file, adjust the rest accordingly.
-
-Usage: zero-bd-chapters.py <file.txt>
+Split a multi-episode Blu-ray chapters file,
+reset the first timestamp in each resulting
+file to zero, adjust the rest accordingly.
 '''
 
-import sys
+import sys, argparse
 
 def timestamp2Seconds(timestamp):
     timeParts = timestamp.split(':')
@@ -19,28 +18,45 @@ def seconds2Timestamp(seconds):
     m = str(int(seconds / 60 % 60)).zfill(2)
     s = str(int(seconds % 60)).zfill(2)
     ms = str(seconds).split('.')[1].zfill(3)
-    return ':'.join((h,m,s))+'.'+ms
+    return ':'.join((h,m,s)) + '.' + ms
 
 def main():
+    parser = argparse.ArgumentParser(description='Split a multi-episode Blu-ray chapter timestamps file to multiple chapter files, adjusted for time.')
+    parser.add_argument('--file', metavar='<path>', help='Input file', required=True)
+    parser.add_argument('--start', metavar='<num>', type=int, help='Start offset -- e.g. 3 will start at the 3rd timestamp', required=False, default=0)
+    parser.add_argument('--chaps', metavar='<num>', type=int, help='Number of chapters per episode', required=True)
+    args = parser.parse_args()
+
+    total_chap = args.chaps
+    in_fname = args.file
+    chapter_set = []
+    all_sets = []
+    offset_ctr = 0
+    start_offset = args.start
+
     try:
-        firstLine = True
-        firstChapter = 0
-        in_fname = sys.argv[1]
-        out_fname = sys.argv[1].split('.')[0]+'_out.txt'
-        with open(in_fname) as inFile, open(out_fname,'w') as outFile:
+        with open(in_fname) as inFile:
             for line in inFile:
-                if firstLine:
-                    firstChapter = timestamp2Seconds(line)
-                    firstLine = False
-                adjustedTime = seconds2Timestamp(
-                    timestamp2Seconds(line)-firstChapter)
-                outFile.write(adjustedTime+'\n')
-        print 'Chapters retimed and written to ' + out_fname
+                if offset_ctr < start_offset:
+                    offset_ctr += 1
+                    continue
+                chapter_set.append(line)
+                if len(chapter_set) == total_chap:
+                    all_sets.append(chapter_set)
+                    chapter_set = []                
+             
+        for chapter_set in all_sets:
+            first_chapter = timestamp2Seconds(chapter_set[0])
+            part = all_sets.index(chapter_set) + 1
+            out_fname = in_fname.split('.')[0] + '_out_' + str(part) + '.txt'
+            with open(out_fname,'w') as outFile:
+                for chapter in chapter_set:
+                    chapter_time = timestamp2Seconds(chapter)
+                    adjusted_time = seconds2Timestamp(chapter_time - first_chapter)
+                    outFile.write(adjusted_time+'\n')
+            print 'Chapters retimed and written to ' + out_fname
     except IOError:
         print 'Please enter a valid filename!'
-        exit()
-    except IndexError:
-        print 'Please enter a file!'
         exit()
 
 if __name__ == "__main__":
